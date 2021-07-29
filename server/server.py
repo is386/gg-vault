33bfa5bc9571ced96d42d9180c78bd4fa44abad6
is386 <20195347+is386@users.noreply.github.com>
2021-07-29 15:25:52 -0400

    add remove endpoint

--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from routes.game import add_game, search
+from routes.game import add_game, remove_game, search
 from routes.user import auth, create, login
 
 # Sets up flask app and routes
@@ -10,9 +10,8 @@ app.add_url_rule("/create", view_func=create, methods=["POST"])
 app.add_url_rule("/auth", view_func=auth, methods=["POST"])
 app.add_url_rule("/search", view_func=search, methods=["POST"])
 app.add_url_rule("/add", view_func=add_game, methods=["POST"])
+app.add_url_rule("/remove", view_func=remove_game, methods=["POST"])
 # TODO: Get games endpoint   - in: user id     out: multiple games and data
-# TODO: Add game endpoint    - in: game id
-# TODO: Remove game endpoint - in: game id
 # TODO: Move game endpoint   - in: game id
 
 
