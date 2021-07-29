db9c8310dcdd2f671ae5829e98df50b6628b9d42
is386 <20195347+is386@users.noreply.github.com>
2021-07-29 14:28:46 -0400

    add search endpoint

--- server/routes/user.py
+++ server/routes/user.py
@@ -15,6 +15,7 @@ with open("env.json") as f:
     env: dict = json.load(f)
 
 path: str = env["db_path"]
+secret: str = env["access_token_secret"]
 
 
 # Checks if the body contains a valid username and password
@@ -27,16 +28,16 @@ def validAcctBody() -> bool:
             len(request.form["password"]) <= pass_max)
 
 
-def create():
+def create() -> Response:
     # Returns 401 if the body does not contain valid data
     if not validAcctBody():
-        return Response("{'error': 'invalid body'}", status=401)
+        return Response("{'error': 'invalid body'}", status=400)
 
     # Returns 401 if the user name is taken
     user: str = str(request.form["username"])
     db: Database = Database(path)
     if db.user_exists(user):
-        return Response("{'error': 'username taken'}", status=401)
+        return Response("{'error': 'username taken'}", status=400)
 
     # Hashes the given password and inserts it into the db
     pw: bytes = request.form["password"].encode("utf-8")
@@ -47,16 +48,16 @@ def create():
     return Response(status=200)
 
 
-def login():
+def login() -> Response:
     # Returns 401 if the body does not contain valid data
     if not validAcctBody():
-        return Response("{'error': 'invalid body'}", status=401)
+        return Response("{'error': 'invalid body'}", status=400)
 
     # Returns 401 if the user name does not exist
     user: str = str(request.form["username"])
     db: Database = Database(path)
     if not db.user_exists(user):
-        return Response("{'error': 'username does not exist'}", status=401)
+        return Response("{'error': 'username does not exist'}", status=400)
 
     # Check if the given password matches whats in the DB
     pw: bytes = request.form["password"].encode("utf-8")
@@ -66,25 +67,25 @@ def login():
 
     # Sign a user token for auth
     token: bytes = jwt.encode(
-        payload={"username": user}, key=env["access_token_secret"])
+        payload={"username": user}, key=secret)
 
     db.close()
     return Response("{'token': %s}" % token.decode("utf-8"), status=200)
 
 
-def auth():
+def auth() -> Response:
     # TODO: Check if header exists
     auth_header = request.headers.get("authorization")
 
     # TODO: Check if auth header has proper format
     token: str = auth_header.split(" ")[1]
-    payload: dict = jwt.decode(token, key=env["access_token_secret"])
+    payload: dict = jwt.decode(token, key=secret)
 
     # TODO Check if payload has username
     user: str = payload["username"]
     db: Database = Database(path)
     if not db.user_exists(user):
-        return Response("{'error': 'username does not exist'}", status=401)
+        return Response("{'error': 'username does not exist'}", status=400)
 
     db.close()
     return Response(status=200)
--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+from routes.game import search
 from routes.user import auth, create, login
 
 # Sets up flask app and routes
@@ -7,6 +8,11 @@ app = Flask(__name__)
 app.add_url_rule("/login", view_func=login, methods=["POST"])
 app.add_url_rule("/create", view_func=create, methods=["POST"])
 app.add_url_rule("/auth", view_func=auth, methods=["POST"])
+app.add_url_rule("/search", view_func=search, methods=["POST"])
+# TODO: Get games endpoint   - in: user id     out: multiple games and data
+# TODO: Add game endpoint    - in: game id
+# TODO: Remove game endpoint - in: game id
+# TODO: Move game endpoint   - in: game id
 
 
 @app.route("/")
