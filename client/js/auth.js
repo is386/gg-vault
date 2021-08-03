462d6fe5dbe83829d3ee9c77c26b1f31f40337a4
is386 <20195347+is386@users.noreply.github.com>
2021-08-03 17:32:37 -0400

    add auth based redirects

---
+++ client/js/auth.js
@@ -0,0 +1,40 @@
+let auth_url = "http://127.0.0.1:8080/auth"
+
+function authHome() {
+    if (!localStorage.ggToken) {
+        location.href="/client/index.html";
+        return;
+    }
+
+    $.ajax({
+        url: auth_url,
+        type: 'POST',
+        dataType: 'json',
+        headers: {"Authorization": "Bearer " + localStorage.ggToken},
+        complete: function(data) {
+            let status = data.status;
+            if (status != 200) {
+                location.href="/client/index.html";
+            }
+        }
+    });
+}
+
+function authLogin() {
+    if (!localStorage.ggToken) {
+        return;
+    }
+
+    $.ajax({
+        url: auth_url,
+        type: 'POST',
+        dataType: 'json',
+        headers: {"Authorization": "Bearer " + localStorage.ggToken},
+        complete: function(data) {
+            let status = data.status;
+            if (status == 200) {
+                location.href="/client/home.html";
+            }
+        }
+    });
+}
