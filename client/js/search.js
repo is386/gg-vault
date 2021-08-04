6fcc9c78eeac66426a4bdcdb3315a8e0d2a5f109
is386 <singh.indervir89@gmail.com>
2021-08-04 17:47:59 -0400

    add buttons to add games

--- client/js/search.js
+++ client/js/search.js
@@ -1,4 +1,5 @@
 let search_url = "http://127.0.0.1:8080/search"
+let add_url = "http://127.0.0.1:8080/add"
 
 function search() {
     let urlParams = new URLSearchParams(window.location.search);
@@ -28,14 +29,39 @@ function populateResults(games) {
         }
         let card = $(`
         <div class="col-auto mb-3">
-            <div id=${g.id} class="card" style="width: 18rem;">
+            <div id=${g.id} class="card text-center" style="width: 18rem;">
                 <img src="${g.cover.url}" class="card-img-top">
                 <div class="card-body">
                     <h5 class="card-title">${g.name}</h5>
                 </div>
             </div>
         </div>`)
+        let myGamesBtn = $("<button class=\"btn btn-success mt-2\"><i class=\"fa fa-plus\"></i> My Games</button>");
+        let wishBtn = $("<button class=\"btn btn-primary mt-2\"><i class=\"fa fa-plus\"></i> Wishlist</button>");
+        myGamesBtn.click(function() {
+            addGame(g.id, 0);
+        });
+        wishBtn.click(function() {
+            addGame(g.id, 1);
+        });
+        card.find(".card-body").append(myGamesBtn).append(wishBtn);
         container.append(card);
     }
+}
 
+function addGame(gameId, wishlist) {
+    if (wishlist) {
+        formData = {"game_id": gameId, "wishlist": 1};
+    } else {
+        formData = {"game_id": gameId};
+    }
+    $.ajax({
+        url: add_url,
+        type: "POST",
+        dataType: "json",
+        data: formData,
+        headers: {"Authorization": "Bearer " + localStorage.ggToken},
+        complete: function(data) {
+        }
+    });
 }
